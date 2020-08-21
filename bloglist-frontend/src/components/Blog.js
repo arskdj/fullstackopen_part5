665c864f1f0b6985f143d9f3b2c02f8cdc7af7e9
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-22 00:46:49 +0300

    5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+
+const Blog = ({ blog }) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const [show, setShow] = useState(false)
+    const buttonName = show ? 'hide' : 'view'
+
+    const toggleDetails = () => {
+        setShow(!show)
+    }
+
+    const blogDetails = () => (
+        <div>
+            <p> { blog.url } </p>
+            <p> { blog.likes } <button> like </button></p>
+            <p> { blog.author } </p>
+        </div>
+    )
+
+    return (
+        <div style={blogStyle}>
+            {blog.title}
+            <button onClick={toggleDetails}> {buttonName} </button>
+            {show && blogDetails()}
+        </div>
+    )
+}
 
 export default Blog
